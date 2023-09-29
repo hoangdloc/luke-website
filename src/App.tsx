a3a84f3bdfccbbcdc154f9a2caeb0093300ad71a
Hoàng Đình Lộc <loc.hd@adamo.software>
2023-09-30 00:28:52 +0700

    ~1 [Other] Init project base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Icon } from './components/icon';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Icon name="react" />
+      <p className="text-red-500">Hello World</p>
+    </div>
+  );
+};
+
+export default App;
